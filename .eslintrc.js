ffa3b579bc75c7862500160d5fd4519edbfb1f3c
Gading Nasution <sutan.gnst@gmail.com>
2022-11-25 00:36:57 +0700

    Init code rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/* eslint-disable comma-dangle */
+
 module.exports = {
   root: true,
   extends: '@react-native-community',
@@ -7,10 +9,50 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
+        semi: ['error', 'always'],
+        indent: ['error', 2, { SwitchCase: 1 }],
+        'comma-dangle': ['error', 'never'],
+        'space-before-blocks': 'error',
+        'no-multiple-empty-lines': ['error', { max: 1 }],
+        'object-curly-spacing': ['error', 'always'],
+        'array-bracket-spacing': 'error',
+        'keyword-spacing': 'error',
+        'arrow-spacing': 'error',
+        'space-infix-ops': 'error',
+        'no-console': 'warn',
+        'react-hooks/exhaustive-deps': 'off',
+        'no-useless-catch': 'off',
+        'jsx-quotes': ['error', 'prefer-double'],
+        'space-before-function-paren': ['error', 'never'],
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/semi': 'error',
+        '@typescript-eslint/type-annotation-spacing': 'error',
+        '@typescript-eslint/space-before-blocks': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/member-delimiter-style': [
+          'error',
+          {
+            multiline: {
+              delimiter: 'semi',
+              requireLast: true
+            },
+            singleline: {
+              delimiter: 'semi',
+              requireLast: true
+            },
+            multilineDetection: 'brackets'
+          }
+        ],
+        '@typescript-eslint/quotes': [
+          'error',
+          'single',
+          {
+            allowTemplateLiterals: true
+          }
+        ]
+      }
+    }
+  ]
 };
