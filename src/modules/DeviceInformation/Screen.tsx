f111d76564e57b0a99e0eb1ca0a34bbddd4431ce
Gading Nasution <sutan.gnst@gmail.com>
2022-11-26 22:32:21 +0700

    wip: challenge 1

--- src/modules/DeviceInformation/Screen.tsx
+++ src/modules/DeviceInformation/Screen.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, View, NativeModules } from 'react-native';
+
+const DeviceInfo = NativeModules.DeviceInfo;
 
 /**
  * 1. Ambil device id dari mobile device menggunakan bridging native Android dan di IOS
  *    (tanpa menggunakan library)
  */
 function DeviceInformationScreen() {
+  useEffect(() => {
+    DeviceInfo.getUniqueID().then((deviceID: string) => {
+      console.log('Current Device Unqiue ID: ' + deviceID);
+    });
+  });
+
   return (
     <View>
       <Text>From Device Information Screen</Text>
